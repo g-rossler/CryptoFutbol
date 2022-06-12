9e4440cebe678173d5ad35239a9de93ebe7de1be
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-06-12 00:41:23 -0300

    Se crea la interfaz de el detalle de los equipos

--- src/ui.js
+++ src/ui.js
@@ -5,8 +5,10 @@ import htmlTorneo from "./pages/torneo";
 import htmlTrasferencias from "./pages/transferencias";
 import htmlBinvenida from "./pages/bienvenida";
 import htmlLogIn from "./pages/login";
+import htmlTorneoDetalle from "./pages/TorneoDetalle";
 import crearEquipos from "../back/crearEquipo";
 import datosTorneo from '../back/torneo'
+import datoJugadoresBrasil from '../back/datoEquipoBrasil'
 
 function crearTablaTorneo() {
   const $tabla = document.querySelector('#tabla')
@@ -36,7 +38,7 @@ function crearTablaTorneo() {
     $boton.innerText = 'DETALLES'
     $boton.className = 'button-81'
     $boton.id = datosTorneo[i].nombre
-    //$boton.addEventListener('click')
+    $boton.addEventListener('click', () => crearInterfaz('detalle equipo torneo'))
 
     $tr.appendChild($th)
     $tr.appendChild($club)
@@ -51,6 +53,67 @@ function crearTablaTorneo() {
     $tbody.appendChild($tr)
   }
   $tabla.appendChild($tbody)
+}
+
+function crearJugadoresTitulares(){
+  const $tabla = document.querySelector('#tabla-titulares')
+  const $tbody = document.createElement('tbody')
+  for(let i = 0; i <= 10; i++){
+    const $tr = document.createElement('tr')
+    const $th = document.createElement('th')
+    $th.scope = 'row'
+    $th.innerText = datoJugadoresBrasil.jugadores.titulares[i].posicion
+    const $nombre = document.createElement('td')
+    $nombre.innerText = datoJugadoresBrasil.jugadores.titulares[i].nombre
+    const $atq = document.createElement('td')
+    $atq.innerText = datoJugadoresBrasil.jugadores.titulares[i].habilidades.ataque
+    const $def = document.createElement('td')
+    $def.innerText = datoJugadoresBrasil.jugadores.titulares[i].habilidades.defensa
+    const $res = document.createElement('td')
+    $res.innerText = datoJugadoresBrasil.jugadores.titulares[i].habilidades.resistencia
+    
+
+    $tr.appendChild($th)
+    $tr.appendChild($nombre)
+    $tr.appendChild($atq)
+    $tr.appendChild($def)
+    $tr.appendChild($res)
+    $tbody.appendChild($tr)
+  }
+  $tabla.appendChild($tbody)
+}
+
+function crearJugadoresSuplentes() {
+  const $tabla = document.querySelector('#tabla-suplentes')
+  const $tbody = document.createElement('tbody')
+  for(let i = 0; i <= 4; i++){
+    const $tr = document.createElement('tr')
+    const $th = document.createElement('th')
+    $th.scope = 'row'
+    $th.innerText = datoJugadoresBrasil.jugadores.suplentes[i].posicion
+    const $nombre = document.createElement('td')
+    $nombre.innerText = datoJugadoresBrasil.jugadores.suplentes[i].nombre
+    const $atq = document.createElement('td')
+    $atq.innerText = datoJugadoresBrasil.jugadores.suplentes[i].habilidades.ataque
+    const $def = document.createElement('td')
+    $def.innerText = datoJugadoresBrasil.jugadores.suplentes[i].habilidades.defensa
+    const $res = document.createElement('td')
+    $res.innerText = datoJugadoresBrasil.jugadores.suplentes[i].habilidades.resistencia
+    
+
+    $tr.appendChild($th)
+    $tr.appendChild($nombre)
+    $tr.appendChild($atq)
+    $tr.appendChild($def)
+    $tr.appendChild($res)
+    $tbody.appendChild($tr)
+  }
+  $tabla.appendChild($tbody)
+}
+
+function crearEquipoDetalle(){
+  crearJugadoresTitulares()
+  crearJugadoresSuplentes()
 }
 
 function crearBotones() {
@@ -83,7 +146,12 @@ function crearInterfaz(pantalla) {
     $aplicacion.innerHTML = htmlTorneo;
     crearBotones();
     crearTablaTorneo()
-  } else if (pantalla === "transferencias") {
+  } else if (pantalla === "detalle equipo torneo") {
+    const $aplicacion = document.querySelector("#main");
+    $aplicacion.innerHTML = htmlTorneoDetalle;
+    crearBotones();
+    crearEquipoDetalle()
+  }else if (pantalla === "transferencias") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlTrasferencias;
     crearBotones();
