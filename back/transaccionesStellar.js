f6886c2fa72f762ede532a743ed41d184ba6adae
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-06-12 16:49:04 -0300

    Crea la operacion de enviar el premio del torneo

--- back/transaccionesStellar.js
+++ back/transaccionesStellar.js
@@ -5,6 +5,8 @@ const sdk = /** @type {import("stellar-sdk")} */ (window.StellarSdk);
 const { Keypair, Asset, Server, TransactionBuilder, Operation } = sdk;
 const server = new Server("https://horizon-testnet.stellar.org");
 
+
+//GCGBF6WKMVLVBDGMXOYNQNLGEUSN2ASIKZWQUCBOLDVMVUE4ZWJTZ3FC
 const ditribuidorKeyPair = sdk.Keypair.fromSecret(
   "SCMXWSZXDJKG7TXQOJZ5ZDJBQ5H7CYFMSQGJWPTLRACHPYMPLV3K2PWY"
 );
@@ -14,10 +16,16 @@ function consultarDatosUsuario() {
   return datosUsuario
 }
 
-export default async function comprarJugador() {
+export async function comprarJugador() {
   const datosUsuario = consultarDatosUsuario();
   await realizarPago(datosUsuario);
   crearInterfaz('transferencia exitosa')
+}
+
+export async function entregarPremio() {
+  const datosUsuario = consultarDatosUsuario();
+  await enviarPremio(datosUsuario);
+  crearInterfaz('premio')
 }
 
 async function realizarPago(datosUsuario) {
@@ -39,8 +47,33 @@ async function realizarPago(datosUsuario) {
     tx.sign(usuarioKeyPair);
 
     try {
-        const txResult = await server.submitTransaction(tx);
+        const txResultados = await server.submitTransaction(tx);
     } catch (e) {
         console.error(e);
     }
+}
+
+export async function enviarPremio(datosUsuario) {
+  const keySecretaUsuario = datosUsuario.keySecreta
+  const usuarioKeyPair = Keypair.fromSecret(keySecretaUsuario);
+  const cuentaOrigen = await server.loadAccount(ditribuidorKeyPair.publicKey());
+
+  const tx = new TransactionBuilder(cuentaOrigen, {
+      fee: await server.fetchBaseFee(),
+      networkPassphrase: "Test SDF Network ; September 2015",
+  }).addOperation(Operation.payment({
+      amount: "1",
+      asset: Asset.native(),
+      destination: usuarioKeyPair.publicKey()
+  }))
+      .setTimeout(60 * 10)
+      .build();
+
+  tx.sign(cuentaOrigen);
+
+  try {
+      const txResultados = await server.submitTransaction(tx);
+  } catch (e) {
+      console.error(e);
+  }
 }
---
+++ src/pages/premio.js
@@ -0,0 +1,50 @@
+const urlEscudo = new URL ("../imagenes/escudos/escudo.svg", import.meta.url);
+const urlLogo = new URL ("../imagenes/logo.svg", import.meta.url);
+const urlArrow = new URL ("../imagenes/arrow.svg", import.meta.url);
+
+const htmlPremio =`<div id="aplicacion" class="container-fluid d-flex flex-column justify-content-between p-0">
+<div class="container-fluid d-flex flex-row justify-content-between" id="navbar">
+  <div class="d-flex flex-row align-items-center justify-content-center px-4 py-1 bor-der" id="logo">
+    <img src="${urlLogo}" alt="" class="" />
+    <p class="text-white text-center m-0 ps-3 fs-3 fw-bold">CryptoFutbol</p>
+  </div>
+
+  <div class="d-flex flex-row align-items-center justify-content-center px-4 py-1">
+    <p class="text-white text-center m-0 ps-3 fs-6 fw-bold">
+      CUENTA 4064646465
+    </p>
+
+    <p class="text-white text-center m-0 ps-3 fs-6 fw-bold">1000$FTOK</p>
+  </div>
+
+  <div class="d-flex flex-row align-items-center justify-content-between px-4 py-1" id="siguiente-fecha">
+    <p class="text-white text-center m-0 ps-3 fs-6 fw-bold">FECHA 9</p>
+
+    <div>
+      <img src="${urlArrow}" alt="" class="ps-1" />
+    </div>
+  </div>
+</div>
+
+
+<div
+        id="aplicacion"
+        class="container-fluid d-flex flex-column justify-content-between p-0"
+      >
+        <div id="principal" style="height: 100vh;" class="d-flex flex-column justify-content-center align-items-center">
+          <h1 class="text-center text-white mb-5 display-1">Felicidades saliste primero en el torneo, ganaste 20 $FTOK!!!</h1>
+        </div>
+      </div>
+
+
+
+<div id="menu-botones" class="container-fluid d-flex flex-row justify-content-center py-3">
+  <button class="button-81 mx-2" role="button" id='boton-inicio'>INICIO</button>
+  <button class="button-81 mx-2" role="button" id='boton-equipo'>EQUIPO</button>
+  <button class="button-81 mx-2" role="button" id='boton-torneo'>TORNEO</button>
+  <button class="button-81 mx-2" role="button" id='boton-club'>CLUB</button>
+  <button class="button-81 mx-2" role="button" id='boton-transferencias'>TRANSFERENCIAS</button>
+</div>
+</div>`
+
+export default htmlPremio
