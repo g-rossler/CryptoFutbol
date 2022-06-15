6cfba5476cffb12b626a1b71012e2484afc1c62e
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-06-15 13:39:49 -0300

    Modificar la funcion del inicio
    Se crea un index y se separan las funciones para cumplir con los principios SOLID

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
-import * as bootstrap from 'bootstrap';
-import crearInterfaz from './ui'
-import crearEquipos from '../back/crearEquipo';
+import crearInterfaz from "./ui";
+import { guardarDatosUsuarioLocalStorage } from "./localStorage";
+import { crearCompraInicialToken } from "../back/transaccionesStellar";
 
-
-function main() {
-    crearInterfaz('')
+export default function inicio() {
+  crearInterfaz("login");
 }
 
-main()
+export async function crearJuego() {
+  const datosFormulario = {
+    nombreClub: document.querySelector("#input-nombre").value,
+    keyPublica: document.querySelector("#key-publica").value,
+    cantidadXLM: document.querySelector("#cantidad-XLM").value,
+  };
+  guardarDatosUsuarioLocalStorage(datosFormulario);
+  await crearCompraInicialToken();
+  //await crearEquipos(datosEquipo);
+
+  //crearInterfaz("menu de inicio");
+}
