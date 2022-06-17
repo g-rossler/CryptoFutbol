55d19b51c878b1b305498fe131635cee201d77fc
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-06-17 11:09:43 -0300

    Crear nuevas funciones de localStorage
    Se crean funcion para guardar datos de FTOK

--- src/localStorage.js
+++ src/localStorage.js
@@ -8,8 +8,17 @@ export function guardarDatosUsuarioLocalStorage(datos) {
   localStorage.setItem("DatosUsuario", datosUsuario);
 }
 
+export function guardarFTOKUsuarioLocalStorage(datos) {
+  const datosUsuario = JSON.stringify(datos);
+  localStorage.setItem("FTOK", datosUsuario);
+}
 
 export function consultarDatosUsuarioLocalStorage() {
   const datosUsuario = JSON.parse(localStorage.getItem("DatosUsuario"));
+  return datosUsuario;
+}
+
+export function consultarFTOKUsuarioLocalStorage() {
+  const datosUsuario = JSON.parse(localStorage.getItem("FTOK"));
   return datosUsuario;
 }
