b68d113d441f9232aebffb7d076bb0a5cbae2e8f
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-06-17 11:10:54 -0300

    Crear funcion para verificar saldo FTOK

--- src/ui.js
+++ src/ui.js
@@ -7,10 +7,12 @@ import htmlLogIn from "./pages/login";
 import htmlTorneoDetalle from "./pages/TorneoDetalle";
 import htmlTransferenciaExitosa from "./pages/tranferenciaExitosa";
 import htmlPremio from "./pages/premio";
-import crearEquipos from "../back/crearEquipo";
 import datosTorneo from "../back/torneo";
 import datoJugadoresBrasil from "../back/datoEquipoBrasil";
-import { comprarJugador, entregarPremio, crearToken } from "../back/transaccionesStellar";
+import {
+  comprarJugador,
+  entregarPremio,
+} from "../back/transaccionesStellar";
 import { crearJuego } from "./app";
 
 function crearBotonCompra() {
@@ -21,7 +23,7 @@ function crearBotonCompra() {
 function crearTablaTorneo() {
   const $tabla = document.querySelector("#tabla");
   const $tbody = document.createElement("tbody");
-  $tbody.id = 'tbody'
+  $tbody.id = "tbody";
   for (let i = 0; i <= 9; i++) {
     const $tr = document.createElement("tr");
     const $th = document.createElement("th");
@@ -79,14 +81,11 @@ function crearJugadoresTitulares(baseDatos) {
     const $nombre = document.createElement("td");
     $nombre.innerText = baseDatos.jugadores.titulares[i].nombre;
     const $atq = document.createElement("td");
-    $atq.innerText =
-    baseDatos.jugadores.titulares[i].habilidades.ataque;
+    $atq.innerText = baseDatos.jugadores.titulares[i].habilidades.ataque;
     const $def = document.createElement("td");
-    $def.innerText =
-    baseDatos.jugadores.titulares[i].habilidades.defensa;
+    $def.innerText = baseDatos.jugadores.titulares[i].habilidades.defensa;
     const $res = document.createElement("td");
-    $res.innerText =
-    baseDatos.jugadores.titulares[i].habilidades.resistencia;
+    $res.innerText = baseDatos.jugadores.titulares[i].habilidades.resistencia;
 
     $tr.appendChild($th);
     $tr.appendChild($nombre);
@@ -109,14 +108,11 @@ function crearJugadoresSuplentes(baseDatos) {
     const $nombre = document.createElement("td");
     $nombre.innerText = baseDatos.jugadores.suplentes[i].nombre;
     const $atq = document.createElement("td");
-    $atq.innerText =
-    baseDatos.jugadores.suplentes[i].habilidades.ataque;
+    $atq.innerText = baseDatos.jugadores.suplentes[i].habilidades.ataque;
     const $def = document.createElement("td");
-    $def.innerText =
-    baseDatos.jugadores.suplentes[i].habilidades.defensa;
+    $def.innerText = baseDatos.jugadores.suplentes[i].habilidades.defensa;
     const $res = document.createElement("td");
-    $res.innerText =
-    baseDatos.jugadores.suplentes[i].habilidades.resistencia;
+    $res.innerText = baseDatos.jugadores.suplentes[i].habilidades.resistencia;
 
     $tr.appendChild($th);
     $tr.appendChild($nombre);
@@ -134,13 +130,12 @@ function crearEquipoDetalle(baseDatos) {
 }
 
 function verificarSaldo() {
-  const datosUsuario = JSON.parse(localStorage.getItem("ClubUsuario"));
-  document.querySelector('#ftok-saldo').innerText = `${datosUsuario.cantidadXLM} $FTOK` 
+  const datosUsuario = JSON.parse(localStorage.getItem("FTOK"));
+  document.querySelector("#ftok-saldo").innerText = `${datosUsuario} $FTOK`;
 }
 
 function crearBotones() {
-  document.querySelector("#siguiente-fecha").onclick = () =>
-  entregarPremio();
+  document.querySelector("#siguiente-fecha").onclick = () => entregarPremio();
   document.querySelector("#boton-inicio").onclick = () =>
     crearInterfaz("menu de inicio");
   document.querySelector("#boton-equipo").onclick = () =>
@@ -161,54 +156,56 @@ export function crearInterfaz(pantalla) {
     document.querySelector(
       "#nombre-club"
     ).innerText = `BIENVENIDO ${datosUsuario.nombre}`;
-    verificarSaldo()
+    verificarSaldo();
   } else if (pantalla === "equipo") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlEquipo;
     crearBotones();
-    const datosUsuario = JSON.parse(localStorage.getItem("ClubUsuario"))
-    crearEquipoDetalle(datosUsuario)
-    verificarSaldo()
+    const datosUsuario = JSON.parse(localStorage.getItem("ClubUsuario"));
+    crearEquipoDetalle(datosUsuario);
+    verificarSaldo();
   } else if (pantalla === "club") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlClub;
     crearBotones();
-    document.querySelector('#club-nombre-club').innerText = `${datosUsuario.nombre}`
-    document.querySelector('#club-id').innerText = `${datosUsuario.keyPublica}`
-    verificarSaldo()
+    document.querySelector(
+      "#club-nombre-club"
+    ).innerText = `${datosUsuario.nombre}`;
+    document.querySelector("#club-id").innerText = `${datosUsuario.keyPublica}`;
+    verificarSaldo();
   } else if (pantalla === "premio") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlPremio;
     crearBotones();
-    verificarSaldo()
+    verificarSaldo();
   } else if (pantalla === "torneo") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlTorneo;
     crearBotones();
     crearTablaTorneo();
-    verificarSaldo()
+    verificarSaldo();
   } else if (pantalla === "detalle equipo torneo") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlTorneoDetalle;
     crearBotones();
     crearEquipoDetalle(datoJugadoresBrasil);
-    verificarSaldo()
+    verificarSaldo();
   } else if (pantalla === "transferencias") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlTrasferencias;
     crearBotones();
     crearBotonCompra();
-    verificarSaldo()
+    verificarSaldo();
   } else if (pantalla === "transferencia exitosa") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlTransferenciaExitosa;
     crearBotones();
-    verificarSaldo()
+    verificarSaldo();
   } else if (pantalla === "login") {
     const $aplicacion = document.querySelector("#main");
     $aplicacion.innerHTML = htmlLogIn;
     document.querySelector("#boton-siguiente").onclick = () => {
-      crearJuego()
+      crearJuego();
     };
   }
 }
